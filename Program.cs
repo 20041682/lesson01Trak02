683031ea71233d6bdd48d38daa813391e5d041d5
Alexandra <SHUKLINA_ALEX@MAIL.RU>
2022-11-22 00:26:46 +0300

    начали писать код

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a = 57;
+int b = 6;
+int c = 158;
+int max = a;
+{
+    if (b > max ) b = max;
+    if (c > max ) c = max;
+}
+ Console.WriteLine (max);
